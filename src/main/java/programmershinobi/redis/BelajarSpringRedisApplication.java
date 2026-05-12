3c5fbbd365452e5c23a072605a8f03cdf8dedc67
programmerShinobi <shinobiprogrammer@gmail.com>
2026-05-12 14:18:03 +0700

    [add] setup project

---
+++ src/main/java/programmershinobi/redis/BelajarSpringRedisApplication.java
@@ -0,0 +1,13 @@
+packagt e programmershinobi.redis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelajarSpringRedisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BelajarSpringRedisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/programmershinobi/redis/BelajarSpringRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package programmershinobi.redis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BelajarSpringRedisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
