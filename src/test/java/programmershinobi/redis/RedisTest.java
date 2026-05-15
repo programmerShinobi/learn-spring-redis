31f144574f65a306c555b97ddc6bb4f46152279a
programmerShinobi <shinobiprogrammer@gmail.com>
2026-05-15 09:06:58 +0700

    [update] learn : list operation

---
+++ src/test/java/programmershinobi/redis/RedisTest.java
@@ -0,0 +1,60 @@
+package programmershinobi.redis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.ListOperations;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+
+import java.time.Duration;
+
+import static org.springframework.test.web.servlet.MockMvcBuilder.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.hamcrest.MatcherAssert.*;
+import static org.hamcrest.Matcher.*;
+import static org.mockito.Mockito.*;
+
+@SpringBootTest
+public class RedisTest {
+
+    @Autowired
+    private StringRedisTemplate redisTemplate;
+
+    @Test
+    void redisTemplate() {
+        assertNotNull(redisTemplate);
+    }
+
+    @Test
+    void string() throws InterruptedException {
+
+        ValueOperations<String, String> operations = redisTemplate.opsForValue();
+
+        operations.set("name", "Faqih", Duration.ofSeconds(2));
+        assertEquals("Faqih", operations.get("name"));
+
+        Thread.sleep(Duration.ofSeconds(3));
+        assertNull(operations.get("name"));
+
+    }
+
+    @Test
+    void list() {
+        ListOperations<String, String> operations = redisTemplate.opsForList();
+
+        operations.rightPush("names", "Faqih");
+        operations.rightPush("names", "Pratama");
+        operations.rightPush("names", "Muhti");
+
+        assertEquals("Faqih", operations.leftPop("names"));
+        assertEquals("Pratama", operations.leftPop("names"));
+        assertEquals("Muhti", operations.leftPop("names"));
+
+    }
+
+}
