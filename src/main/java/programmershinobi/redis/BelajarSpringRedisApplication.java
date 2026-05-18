57d0f93e90724ee416c84c17a9da72b52f51d68c
programmerShinobi <shinobiprogrammer@gmail.com>
2026-05-18 08:56:15 +0700

    [update] learn : stream listener

--- src/main/java/programmershinobi/redis/BelajarSpringRedisApplication.java
+++ src/main/java/programmershinobi/redis/BelajarSpringRedisApplication.java
@@ -1,10 +1,63 @@
 package programmershinobi.redis;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.stream.Consumer;
+import org.springframework.data.redis.connection.stream.ObjectRecord;
+import org.springframework.data.redis.connection.stream.ReadOffset;
+import org.springframework.data.redis.connection.stream.StreamOffset;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.stream.StreamMessageListenerContainer;
+import org.springframework.data.redis.stream.Subscription;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.time.Duration;
 
 @SpringBootApplication
+@EnableScheduling
+@Slf4j
 public class BelajarSpringRedisApplication {
+
+	@Autowired
+	private StringRedisTemplate redisTemplate;
+
+	@Bean
+	public Subscription orderSubscription(StreamMessageListenerContainer<String, ObjectRecord<String, Order>> orderContainer,
+										  OrderListener orderListener) {
+
+		try {
+			redisTemplate.opsForStream().createGroup("orders", "my-group");
+		} catch (Throwable throwable) {
+
+		}
+
+		var offset = StreamOffset.create("orders", ReadOffset.lastConsumed());
+		var consumer = Consumer.from("my-group", "consumer-1");
+		var readRequest = StreamMessageListenerContainer.StreamReadRequest
+				.builder(offset)
+				.consumer(consumer)
+				.autoAcknowledge(false)
+				.cancelOnError(throwable -> false)
+				.errorHandler(throwable -> log.warn(throwable.getMessage()))
+				.build();
+
+		return orderContainer.register(readRequest, orderListener);
+	}
+
+	@Bean(destroyMethod = "stop",  initMethod = "start")
+	public StreamMessageListenerContainer<String, ObjectRecord<String, Order>> orderContainter(RedisConnectionFactory connectionFactory) {
+		var options = StreamMessageListenerContainer.StreamMessageListenerContainerOptions
+				.builder()
+				.pollTimeout(Duration.ofSeconds(5))
+				.targetType(Order.class)
+				.build();
+
+		return StreamMessageListenerContainer.create(connectionFactory, options);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(BelajarSpringRedisApplication.class, args);
---
+++ src/main/java/programmershinobi/redis/Order.java
@@ -0,0 +1,16 @@
+package programmershinobi.redis;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Order {
+
+    private String id;
+
+    private Long amount;
+
+}
